f2a57fb1de9a62988910ca5a82d4a2253864e498
rebeccanpeng <rebeccanpeng@gmail.com>
2025-11-25 17:53:37 -0500

    Add Kotlin gitignore file

--- app/src/main/java/com/example/weatherapp/CityPickerFragment.kt
+++ app/src/main/java/com/example/weatherapp/CityPickerFragment.kt
@@ -1,5 +1,20 @@
 package com.example.weatherapp
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
-class CityPickerFragment : Fragment(R.layout.city_picker)
+class CityPickerFragment : Fragment(R.layout.city_picker) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewModel: CityPickerViewModel by viewModels()
+        lifecycleScope.launch {
+            viewModel.uiState.collect {
+                // Update UI
+            }
+        }
+    }
+}
