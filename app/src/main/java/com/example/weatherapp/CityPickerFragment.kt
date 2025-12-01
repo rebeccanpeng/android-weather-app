772145d1d1eed32821c682a238c3b13a830a6d52
rebeccanpeng <rebeccanpeng@gmail.com>
2025-12-01 10:56:08 -0500

    add viewmodel

--- app/src/main/java/com/example/weatherapp/CityPickerFragment.kt
+++ app/src/main/java/com/example/weatherapp/CityPickerFragment.kt
@@ -1,5 +1,28 @@
 package com.example.weatherapp
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 
-class CityPickerFragment : Fragment(R.layout.city_picker)
+class CityPickerFragment : Fragment(R.layout.city_picker) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewModel: CityPickerViewModel by viewModels()
+        viewLifecycleOwner.lifecycleScope.launch {
+            // 1. Coroutine launched here lives until Fragment.onDestroy()
+
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // 2. This block runs only between STARTED and STOPPED
+                viewModel.uiState.collect {
+                    // Update UI safely
+                }
+            }
+            // 3. This line won't execute until the Fragment is DESTROYED
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/CityPickerViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class CityPickerViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow<List<Int>>((0..100).toList())
+    val uiState: StateFlow<List<Int>> = _uiState
+
+}
